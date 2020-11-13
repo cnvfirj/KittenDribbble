7806569b71df244ac0c8ec4f86165add87724ee6
cnvfirj <cnvfirj@gmail.com>
2020-11-13 16:39:26 +0300

    AndroidViewModel update example

--- app/src/main/java/com/kittendevelop/kittendribbble/ui/register/RegisterFragment.java
+++ app/src/main/java/com/kittendevelop/kittendribbble/ui/register/RegisterFragment.java
@@ -1,7 +1,9 @@
 package com.kittendevelop.kittendribbble.ui.register;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.kittendevelop.kittendribbble.R;
 import com.kittendevelop.kittendribbble.ui.ViewModelFactory;
@@ -18,6 +22,10 @@ import com.kittendevelop.kittendribbble.ui.ViewModelFactory;
 public class RegisterFragment extends Fragment {
 
     private RegisterViewModel mViewModel;
+
+    private TextView mTextPreview;
+    private ImageView mLogo;
+    private ImageView mMark;
 
     public static RegisterFragment newInstance() {
         return new RegisterFragment();
@@ -27,6 +35,7 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.register_fragment, container, false);
     }
 
@@ -35,7 +44,33 @@ public class RegisterFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this, new ViewModelFactory(new RegisterViewModel(getActivity().getApplication())))
                 .get(RegisterViewModel.class);
+//        getLifecycle().addObserver(mViewModel);
+        mTextPreview = getView().findViewById(R.id.register_preview);
+        mLogo = getView().findViewById(R.id.register_logo_dribbble);
+        mMark = getView().findViewById(R.id.register_mark_dribbble);
+        applyData();
+    }
 
+    private void applyData(){
+         mViewModel.getPreviewMassage().observe(this, new Observer<String>() {
+             @Override
+             public void onChanged(String s) {
+                 mTextPreview.setText(s);
+             }
+         });
+         mViewModel.getLogo().observe(this, new Observer<Drawable>() {
+             @Override
+             public void onChanged(Drawable drawable) {
+                 mLogo.setImageDrawable(drawable);
+             }
+         });
+
+         mViewModel.getMark().observe(this, new Observer<Drawable>() {
+             @Override
+             public void onChanged(Drawable drawable) {
+                 mMark.setImageDrawable(drawable);
+             }
+         });
     }
 
 }
--- app/src/main/java/com/kittendevelop/kittendribbble/ui/register/RegisterViewModel.java
+++ app/src/main/java/com/kittendevelop/kittendribbble/ui/register/RegisterViewModel.java
@@ -7,12 +7,19 @@ import android.graphics.drawable.Drawable;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ViewModel;
 
 import com.kittendevelop.kittendribbble.R;
 
 import org.jetbrains.annotations.Contract;
 
+import static androidx.lifecycle.Lifecycle.Event.ON_ANY;
+import static com.kittendevelop.kittendribbble.ui.help.Massages.MASSAGE;
 
 
 public class RegisterViewModel extends AndroidViewModel {
@@ -30,42 +37,37 @@ public class RegisterViewModel extends AndroidViewModel {
     private final String mID = "8a4e6b1c8d3adca5d887b45a2147c48c11a3e4361ce5adef08964228413644a0";
     private final String mSecret = "4fc3c445569dbfe65998c470e88cc05fcd4872079ca1f2191eefec133895224e";
 
-    private String mPreviewMassage;
-    private Drawable mMark;
-    private Drawable mLogo;
+    private MutableLiveData<String> mPreviewMassage = new MutableLiveData<>();
+    private MutableLiveData<Drawable> mMark = new MutableLiveData<>();
+    private MutableLiveData<Drawable> mLogo = new MutableLiveData<>();
 
     public RegisterViewModel(@NonNull Application application) {
         super(application);
+        initVars();
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
     private void initVars(){
-        mPreviewMassage = getApplication().getString(R.string.register_preview);
-        mMark = getApplication().getDrawable(R.drawable.ic_dribbble_ball_mark);
-        mLogo = getApplication().getDrawable(R.drawable.ic_dribbble_logo);
+        mPreviewMassage.setValue(getApplication().getString(R.string.register_preview));
+        mMark.setValue(getApplication().getDrawable(R.drawable.ic_dribbble_ball_mark));
+        mLogo.setValue(getApplication().getDrawable(R.drawable.ic_dribbble_logo));
     }
 
-    public String getPreviewMassage() {
+//    @OnLifecycleEvent(ON_ANY)
+//    private void s(LifecycleOwner source, Lifecycle.Event event){
+//        MASSAGE("RegisterViewModel "+event.name());
+//    }
+
+
+    public MutableLiveData<String> getPreviewMassage() {
         return mPreviewMassage;
     }
 
-    public void setPreviewMassage(String mPreviewMassage) {
-        this.mPreviewMassage = mPreviewMassage;
-    }
-
-    public Drawable getMark() {
+    public MutableLiveData<Drawable> getMark() {
         return mMark;
     }
 
-    public void setMark(Drawable mMark) {
-        this.mMark = mMark;
-    }
-
-    public Drawable getLogo() {
+    public MutableLiveData<Drawable> getLogo() {
         return mLogo;
-    }
-
-    public void setLogo(Drawable mLogo) {
-        this.mLogo = mLogo;
     }
 }
