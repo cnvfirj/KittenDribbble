e73f9003a2dc347936c02f902f4e9d3d048e5f16
cnvfirj <cnvfirj@gmail.com>
2020-11-19 14:31:24 +0300

    Correct win reg

--- app/src/main/java/com/kittendevelop/kittendribbble/ui/main/MainFragment.java
+++ app/src/main/java/com/kittendevelop/kittendribbble/ui/main/MainFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,6 +35,7 @@ public class MainFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 //        mViewModel = new ViewModelProvider(this, new ViewModelFactory(new MainViewModel(getActivity().getApplication())))
 //                        .get(MainViewModel.class);
     }
--- app/src/main/java/com/kittendevelop/kittendribbble/ui/register/RegisterModel.java
+++ app/src/main/java/com/kittendevelop/kittendribbble/ui/register/RegisterModel.java
@@ -2,22 +2,6 @@ package com.kittendevelop.kittendribbble.ui.register;
 
 public class RegisterModel {
 
-    private final String mAuthorize = "https://dribbble.com/oauth/authorize?" +
-            "client_id=8a4e6b1c8d3adca5d887b45a2147c48c11a3e4361ce5adef08964228413644a0&" +
-            "scope=public+upload";
-//            "redirect_uri=kittencreativity://callback/application-code";
-
-    private final String mDribbble = "https://dribbble.com";
-    private final String mSignUp = "https://dribbble.com/signup/new";
-
-
-    private final String mLinkApp = "https://dribbble.com/account/applications/59798";
-
-    private final String mDribbbleCallbackURL = "kittencreativity://callback";
-    private final String mClientID = "8a4e6b1c8d3adca5d887b45a2147c48c11a3e4361ce5adef08964228413644a0";
-    private final String mClientSecret = "4fc3c445569dbfe65998c470e88cc05fcd4872079ca1f2191eefec133895224e";
-
-
     public String getAuthorize() {
         return "https://dribbble.com/oauth/authorize?" +
                 "client_id=8a4e6b1c8d3adca5d887b45a2147c48c11a3e4361ce5adef08964228413644a0&" +
