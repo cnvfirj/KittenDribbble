a12686618beda5f3cb751b4cddc5a74833f4ddd0
Admin <cnvfirj@gmail.com>
2020-11-22 17:37:59 +0300

    window register

--- app/src/main/java/com/kittendevelop/kittendribbble/ui/main/requests/DribbbleRequests.java
+++ app/src/main/java/com/kittendevelop/kittendribbble/ui/main/requests/DribbbleRequests.java
@@ -1,15 +1,12 @@
 package com.kittendevelop.kittendribbble.ui.main.requests;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface DribbbleRequests {
 
-    @GET("oauth/token?")
+    @POST("oauth/token?")
     Call<DribbbleResponseToken> callToken(
             @Query("client_id") String id,
             @Query("client_secret") String secret,
